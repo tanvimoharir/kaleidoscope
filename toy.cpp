79d3ef075ee78ced29cb746904aa243a95f255b9
Tanvi Moharir <tanvimoharir@gmail.com>
2023-04-24 17:33:39 +0530

    added AST

--- toy.cpp
+++ toy.cpp
@@ -78,3 +78,76 @@ static int gettok() {
 	LastChar = getchar();
 	return ThisChar;
 }
+
+//===----------------------------------
+//Abstract Syntax Tree (aka Parse Tree)
+//===----------------------------------
+
+//namespace is similar to static for variable meaning
+//classes defined under it are only available within this 
+//trnaslation unit
+
+namespace {
+
+/// ExprAST - Base class for all expressions nodes
+class ExprAST {
+public:
+	virtual ~ExprAST() = default;
+};
+
+///NumberExprAST  - Expression class for numeric literal like "1.0"
+class NumberExprAST : public ExprAST {
+	double Val;
+
+public:
+	NumberExprAST(double Val) : Val(Val) {}
+};
+
+///VariableExprAST - Expression class for referencing a variable like "a"
+class VariableExprAST : public ExprAST {
+	std::string Name;
+
+public:
+	VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+/// BinaryExprAST - Expression class for a binary operator
+
+class BinaryExprAST : public ExprAST {
+	char Op;
+	std::unique_ptr<ExprAST> LHS, RHS;
+public:
+	BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS) : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+	std::string Callee;
+	std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+	CallExprAST(const std::string& Callee, std::vector<std::unique_ptr<ExprAST>> Args) : Callee(Callee), Args(std::move(Args)) {}
+};
+
+/// PrototypeAST - This class represents the prototype for a func,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the func takes).
+class PrototypeAST{
+	std::string Name;
+	std::vector<std::string> Args;
+
+public:
+	PrototypeAST(const std::string& Name, std::vector<std::string> Args) : Name(Name), Args(std::move(Args)) {}
+
+	const std::string& getName() const { return Name; }
+};
+
+/// FunctionAST - this class represents a function definition itself
+class FunctionAST {
+	std::unique_ptr<PrototypeAST> Proto;
+	std::unique_ptr<ExprAST> Body;
+public:
+	FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+}// end anonymous namespace
