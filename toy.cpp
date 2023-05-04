657aaab1a7f361f1691535a8f3070211f6037d13
Tanvi Moharir <tanvimoharir@gmail.com>
2023-05-04 16:10:49 +0530

    adding library externed funcitons

--- toy.cpp
+++ toy.cpp
@@ -863,6 +863,28 @@ static void MainLoop() {
 	}
 }
 
+//===----------------------------------------------------
+// Library functions that can be externed from user code
+//===----------------------------------------------------
+
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+///putchard - putchar that takes a double and returns 0
+extern "C" DLLEXPORT double putchard(double X) {
+	fputc((char)X, stderr);
+	return 0;
+}
+
+///printd - printf that takes double and prints it as "%f\n", returning 0.
+extern "C" DLLEXPORT double printd(double X) {
+	fprintf(stderr, "%f\n", X);
+	return 0;
+}
+
 
 //===-------------------------------
 // Main driver code
